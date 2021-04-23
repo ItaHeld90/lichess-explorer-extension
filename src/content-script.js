d988a6b3b18700d31653e11fe21854142bdee368
Itamar Held <ita@gmail.com>
2021-04-23 23:38:59 +0300

    copy as uri button

--- src/content-script.js
+++ src/content-script.js
@@ -3,6 +3,25 @@
 
     const tableAddedObserver = new MutationObserver(generalMutationHandler);
     tableAddedObserver.observe(document.body, { childList: true, subtree: true });
+    window.addEventListener('load', addCopyAsURIButton);
+
+    function addCopyAsURIButton() {
+        // add button to copy PGN
+        const copyPGNButton = document.createElement('button');
+        copyPGNButton.innerText = "Copy as URI";
+        copyPGNButton.style.padding = '3px';
+        copyPGNButton.style.backgroundColor = 'rgb(192, 214, 167)';
+        copyPGNButton.style.marginTop = "8px";
+
+        const pgnElem = document.querySelector('.pgn');
+        pgnElem.append(copyPGNButton);
+
+        copyPGNButton.addEventListener('click', () => {
+            const pgnTextArea = document.querySelector('.pgn textarea');
+            const uciMoves = pgnTextArea.value.split(/\d\.\s/).filter(x => x).flatMap(x => x.trim().split(' '));
+            navigator.clipboard.writeText(`[${uciMoves.map(x => `'${x}'`).join(', ')}]`);
+        })
+    }
 
     function generalMutationHandler(mutationList) {
         const shouldExecute = [...mutationList].some(
