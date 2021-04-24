25bc6d5ede1776096cd72766e391f89b96a0df76
Itamar Held <ita@gmail.com>
2021-04-24 04:31:33 +0300

    fix buttons appearance

--- src/copy-button.js
+++ src/copy-button.js
@@ -1,24 +1,93 @@
 (function init() {
-    window.addEventListener('load', addCopyAsURIButton);
+    const pgnContainerSelector = '.pgn';
+    const pgnTextAreaSelector = '.pgn textarea';
+    const copyablesSelector = '.copyables';
+    const buttonsContainerClassName = '__lichess-pgn-buttons-widget__';
 
-    function addCopyAsURIButton() {
-        // add button to copy PGN
+    window.addEventListener('load', addButtons);
+
+    const copyablesAddedObserver = new MutationObserver(handleTopMutations);
+    copyablesAddedObserver.observe(document.body, { childList: true, subtree: true });
+
+    function handleTopMutations(mutations) {
+        const mutationsList = [...mutations];
+
+        const shouldAddButtons = mutationsList.some((mutation) =>
+            [...mutation.addedNodes, ...mutation.removedNodes].some(isCopyablesNode)
+        );
+
+        if (shouldAddButtons) {
+            addButtons();
+        }
+    }
+
+    function isCopyablesNode(node) {
+        return node instanceof HTMLElement && node.matches(copyablesSelector);
+    }
+
+    function addButtons() {
+        const pgnElem = document.querySelector(pgnContainerSelector);
+
+        const buttonsContainer = document.createElement('div');
+        buttonsContainer.classList.add(buttonsContainerClassName);
+        buttonsContainer.style.marginTop = '8px';
+        buttonsContainer.style.display = 'flex';
+
+        const copyPGNButton = createCopyPGNButton();
+        copyPGNButton.style.marginRight = '5px';
+
+        const uploadPGNButton = createUploadPGNButton();
+
+        buttonsContainer.append(copyPGNButton, uploadPGNButton);
+
+        pgnElem.after(buttonsContainer);
+    }
+
+    function createUploadPGNButton() {
+        const uploadPGNButton = document.createElement('button');
+        uploadPGNButton.innerText = 'Upload PGN';
+        uploadPGNButton.style.padding = '3px';
+        uploadPGNButton.style.marginTop = '8px';
+
+        const fileUploader = document.createElement('input');
+        fileUploader.type = 'file';
+        fileUploader.accept = '.pgn';
+
+        fileUploader.addEventListener('change', function handleFiles() {
+            const file = this.files[0];
+            const reader = new FileReader();
+
+            reader.onload = () => {
+                const pgnTextAreaElem = document.querySelector(pgnTextAreaSelector);
+                pgnTextAreaElem.value = reader.result;
+            };
+
+            reader.readAsText(file);
+        });
+
+        uploadPGNButton.addEventListener('click', () => {
+            fileUploader.click();
+        });
+
+        return uploadPGNButton;
+    }
+
+    function createCopyPGNButton() {
         const copyPGNButton = document.createElement('button');
         copyPGNButton.innerText = 'Copy as SAN';
         copyPGNButton.style.padding = '3px';
         copyPGNButton.style.backgroundColor = 'rgb(192, 214, 167)';
         copyPGNButton.style.marginTop = '8px';
 
-        const pgnElem = document.querySelector('.pgn');
-        pgnElem.append(copyPGNButton);
-
         copyPGNButton.addEventListener('click', () => {
-            const pgnTextArea = document.querySelector('.pgn textarea');
+            const pgnTextArea = document.querySelector(pgnTextAreaSelector);
             const uciMoves = pgnTextArea.value
                 .split(/\d\.\s/)
                 .filter((x) => x)
                 .flatMap((x) => x.trim().split(' '));
             navigator.clipboard.writeText(`[${uciMoves.map((x) => `'${x}'`).join(', ')}]`);
         });
+
+        return copyPGNButton;
     }
 })();
